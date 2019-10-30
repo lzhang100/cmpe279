a9377c619a1c8321f7592cb2c0b03188ccd039c3
lzhang100 <im.mr.lester@gmail.com>
2019-10-29 18:36:48 -0700

    checkforexecreturn

--- assignment2/server.c
+++ assignment2/server.c
@@ -16,17 +16,17 @@ int main(int argc, char const *argv[])
     char buffer[1024] = {0}; 
     char *hello = "Hello from server"; 
     uid_t id = 65534;
-       
+
     // Creating socket file descriptor 
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
     { 
         perror("socket failed"); 
         exit(EXIT_FAILURE); 
     } 
-       
+
     // Forcefully attaching socket to the port 8080 
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, 
-                                                  &opt, sizeof(opt))) 
+      &opt, sizeof(opt))) 
     { 
         perror("setsockopt"); 
         exit(EXIT_FAILURE); 
@@ -34,10 +34,10 @@ int main(int argc, char const *argv[])
     address.sin_family = AF_INET; 
     address.sin_addr.s_addr = INADDR_ANY; 
     address.sin_port = htons( PORT ); 
-       
+
     // Forcefully attaching socket to the port 8080 
     if (bind(server_fd, (struct sockaddr *)&address,  
-                                 sizeof(address))<0) 
+       sizeof(address))<0) 
     { 
         perror("bind failed"); 
         exit(EXIT_FAILURE); 
@@ -48,7 +48,7 @@ int main(int argc, char const *argv[])
         exit(EXIT_FAILURE); 
     } 
     if ((new_socket = accept(server_fd, (struct sockaddr *)&address,  
-                       (socklen_t*)&addrlen))<0) 
+     (socklen_t*)&addrlen))<0) 
     { 
         perror("accept"); 
         exit(EXIT_FAILURE); 
@@ -56,13 +56,19 @@ int main(int argc, char const *argv[])
 
     if(fork() == 0){
 	//Set uid to nobody user
-        setuid(id);
-	printf("Child: The real user ID after setting setuid is %d\n", getuid());
-	//Grab the socket and send it as argument to exec
-        char pipe_readend[12];
-        sprintf(pipe_readend, "%i", new_socket);
-        char * argv_list[] = {"./server2", pipe_readend, NULL}; 
-        execvp("./server2", argv_list);
+        if(setuid(id) == 0){
+            printf("Child: The real user ID after setting setuid is %d\n", getuid());
+            //Grab the socket and send it as argument to exec
+            char pipe_readend[12];
+            sprintf(pipe_readend, "%i", new_socket);
+            char * argv_list[] = {"./server2", pipe_readend, NULL}; 
+            if(execvp("./server2", argv_list) == -1){
+                perror("execvp failed");
+            }
+        }else{
+            perror("setuid failed");
+        }
+        
     }else{
         wait(NULL);
         printf("Parent: The real user ID is %d\n", getuid());
